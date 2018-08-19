614fbbe0dd1ef5e1e13e22bd94a43289f7c630c7
Lewis Buckley <lewis-buckley@cookpad.com>
2018-08-19 14:41:02 +0100

    Fix env var

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "admin", password: ENV.fetch("passowrd", "password")
+  http_basic_authenticate_with name: "admin", password: ENV.fetch("PASSWORD", "password")
 end
